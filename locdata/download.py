b7c7186cf8ee6c6ce54fb168052a28e810bd5921
Vít Tuček <vit-tucek@users.noreply.github.com>
2020-03-25 16:36:33 +0100

    fixed destination.type

--- locdata/download.py
+++ locdata/download.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
-typ = 'ORP' # ,ožné hodnoty:  'Kraj', 'Okres', 'ORP', None (to stahne vsechno do jednoho souboru)
+typ = 'ORP' # možné hodnoty:  'Kraj', 'Okres', 'ORP', None (to stahne vsechno do jednoho souboru)
 SNAPSHOT = 12 # Duben 2019, viz https://api.inovujemesdaty.cz/snapshots.csv
-FILE_NAME = 'commuting_relations_4_2019' # příponu csv připojíme automaticky
+FILE_NAME = 'commuting_relations_ORP_4_2019' # příponu csv připojíme automaticky
 BASE_URL = 'https://api.inovujemesdaty.cz/commuting_between_locations.csv' 
 DOWNLOAD_BY = 10 # stahovat 10 lokací najednou
 SLEEP_TIME = 1 # po každém stažení počkat sekundu
@@ -12,26 +12,20 @@ import pandas as pd
 locations = pd.read_csv('locations.csv')
 if typ:
     locations = locations[locations.type == typ]
-    
-# moc velké
-#loc_param = "&".join('location[]=%d' % x for x in locations.id)
-#url = BASE_URL + "commuting_age_genders.csv?snapshot=%s&" % SNAPSHOT + loc_param
-#print(url) 
 
 def chunks(lst, n):
     """Yield successive n-sized chunks from lst."""
     for i in range(0, len(lst), n):
         yield lst[i:i + n]
-        
+
 def make_url(loc_ids):
     loc_param = "&".join('sourceLocation[]=%d' % x for x in loc_ids)
     return BASE_URL + "?snapshot=%s&" % SNAPSHOT + loc_param
-    
 
 import urllib.request
 import os, shutil, time
 if not os.path.isdir('tmp'):
-    os.mkdir('tmp')
+    os.mkdir('tmp')e
 i = 1
 file_names = []
 for loc_ids in chunks(locations.id, DOWNLOAD_BY):
@@ -48,4 +42,5 @@ for loc_ids in chunks(locations.id, DOWNLOAD_BY):
 
 print("Merging...")
 result = pd.concat((pd.read_csv(f, sep=';') for f in file_names), axis=0, ignore_index=True)
+result = result[result['destinationLocation.type'] == typ]
 result.to_csv(FILE_NAME + '.csv')
